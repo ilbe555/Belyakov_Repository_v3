2587b4b6bc1c26b127c7121357e455caaa8cbb89
ilbe555 <165358852+ilbe555@users.noreply.github.com>
2024-04-22 09:04:11 +0300

    1

---
+++ belyakovproject/belyakovproject/belyakovproject.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+float EnterNumberA(float A)
+{
+
+}
+float EnterNumberB(float B)
+{
+
+}
+float OperationMultip(float A, float B, float res)
+{
+
+}
+float OperationDiv(float A, float B, float res)
+{
+
+}
+int main()
+{
+    float A = 0, B = 0, res = 0;
+    int num = -1;
+    cout << "1. Enter a number A" << endl;
+    cout << "2. Enter a number B" << endl;
+    cout << "3. Perform the operation '*'" << endl;
+    cout << "4. Perform the operation '/'" << endl;
+    cout << endl << "Please enter the number of the desired function or 0 if you want to close the app:" << endl;
+    while (num != 0)
+    {
+        cin >> num;
+        switch (num)
+        {
+        case 1:
+            cout << "A = ";
+            A = EnterNumberA(A);
+            cout << endl;
+            break;
+        case 2:
+            cout << "B = ";
+            B = EnterNumberB(B);
+            cout << endl;
+            break;
+        case 3:
+            res = OperationMultip(A, B, res);
+            cout << "A * B = ";
+            cout << res << endl;
+            break;
+        case 4:
+            res = OperationDiv(A, B, res);
+            cout << "A / B = ";
+            cout << res << endl;
+            break;
+        default:
+            num = 0;
+            break;
+        }
+    }
+}

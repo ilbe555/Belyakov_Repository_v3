1ca27f20d1effd424a649c4d2f91409f71c087ff
ilbe555 <165358852+ilbe555@users.noreply.github.com>
2024-06-29 11:40:59 +0300

    done

---
+++ belyakovproject/belyakovproject/belyakovproject.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+float EnterNumberA(float A)
+{
+    cin >> A;
+    return A;
+}
+float EnterNumberB(float B)
+{
+    cin >> B;
+    return B;
+}
+float OperationMultip(float A, float B, float res)
+{
+    res = A * B;
+    return res;
+}
+float OperationDiv(float A, float B, float res)
+{
+    if (B != 0)
+    {
+        res = A / B;
+        return res;
+    }
+    else
+    {
+        cout << "ERROR";
+        return 0;
+    }
+}
+int main()
+{
+    float A = 0, B = 0, res = 0;
+    int num = -1;
+    cout << "1. Enter a number A" << endl;
+    cout << "2. Enter a number B" << endl;
+    cout << "3. Perform the operation '*'" << endl;
+    cout << "4. Perform the operation '/'" << endl;
+    cout << endl << "Please enter the number of the desired function or 0 if you want to close the app:" << endl;
+    while (num != 0)
+    {
+        cin >> num;
+        switch (num)
+        {
+        case 1:
+            cout << "A = ";
+            A = EnterNumberA(A);
+            cout << endl;
+            break;
+        case 2:
+            cout << "B = ";
+            B = EnterNumberB(B);
+            cout << endl;
+            break;
+        case 3:
+            res = OperationMultip(A, B, res);
+            cout << "A * B = ";
+            cout << res << endl;
+            break;
+        case 4:
+            res = OperationDiv(A, B, res);
+            cout << "A / B = ";
+            cout << res << endl;
+            break;
+        default:
+            num = 0;
+            break;
+        }
+    }
+}
---
+++ belyakovproject/paket.bat
@@ -0,0 +1,6 @@
+"C:\VS\Common7\IDE\devenv.exe" belyakovproject.sln /build Debug
+"C:\Program Files\doxygen\bin\doxygen.exe" config
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" d1.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" d2.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" d3.iss
+
